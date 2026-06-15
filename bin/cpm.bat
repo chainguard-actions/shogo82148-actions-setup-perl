f48e3c768f759cb1f2e22178773393c68e2fc63b
actionchain <actionchain@chainguard.dev>
2026-06-15 01:27:56 +0000

    Update shogo82148/actions-setup-perl@v1.39.0

---
+++ bin/cpm.bat
@@ -0,0 +1,14 @@
+@rem = '--*-Perl-*--
+@set "ErrorLevel="
+@if "%OS%" == "Windows_NT" @goto WinNT
+@perl -x -S "%~dp0cpm" %1 %2 %3 %4 %5 %6 %7 %8 %9
+@set ErrorLevel=%ErrorLevel%
+@goto endofperl
+:WinNT
+@perl -x -S "%~dp0cpm" %*
+@set ErrorLevel=%ErrorLevel%
+@if NOT "%COMSPEC%" == "%SystemRoot%\system32\cmd.exe" @goto endofperl
+@if %ErrorLevel% == 9009 @echo You do not have Perl in your PATH.
+@goto endofperl
+:endofperl
+@set "ErrorLevel=" & @goto _undefined_label_ 2>NUL || @"%COMSPEC%" /d/c @exit %ErrorLevel%
